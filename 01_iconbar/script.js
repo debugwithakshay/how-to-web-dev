5095e68b3dc57b75569bfeb2cc0ea72b737a8219
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-03-03 09:28:27 +0530

    01_iconbar

---
+++ 01_iconbar/script.js
@@ -0,0 +1,14 @@
+let menuIcon = document.querySelectorAll(".iconBarBox span");
+
+menuIcon.forEach(function (icon) {
+
+  icon.addEventListener("click", function () {
+    for (let icon of menuIcon) {
+      icon.classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+
+  
+});
+console.log(menuIcon);
